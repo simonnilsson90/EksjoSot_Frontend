753615c637d2b6f3edf839adcb7243795833a4f4
William Karlström <williamcarlstrom1@gmail.com>
2024-03-14 09:40:31 +0100

    Add routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import HomePage from "./views/HomePage";
+import MarketPage from "./views/MarketPage";
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
 function App() {
     return (
-        <div className="App">
-            <HomePage/>
-        </div>
+        <BrowserRouter>
+            {/*/* Add navBar here */}
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="market" element={<MarketPage/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
